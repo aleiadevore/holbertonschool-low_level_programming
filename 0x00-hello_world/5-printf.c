b56e4b54336b8ea181e341f12a1079e5af4aaa6c
Aleia DeVore <2496@holbertonschool.com>
2020-09-17 01:19:54 +0000

    Created script to print line of text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main  - This will print text
+ *
+ * Description: This will print a line of text about programming with C
+ *
+ * Return: 0 for end
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
