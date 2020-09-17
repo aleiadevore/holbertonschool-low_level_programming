89911bba64b6aa30fe3386353292d8ac9b650291
Aleia DeVore <2496@holbertonschool.com>
2020-09-17 01:02:39 +0000

    Updated with comments for Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+  * main - outputs a script as a sentence
+ *
+ * Description: Outputs the puts function
+ *
+ * Return: 0 to signify end
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
