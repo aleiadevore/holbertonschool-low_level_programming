e69281cb37a0cfb6329c45d00713f83c37d37099
Aleia DeVore <2496@holbertonschool.com>
2020-09-18 04:12:19 +0000

    Created script to skip over letters by combining for and if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main  - prints alphabet
+*
+* Description: prints the alphabet in lowercase
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+{
+	if (a != 'e' && a != 'q')
+	putchar(a);
+}
+{
+	putchar('\n');
+}
+	return (0);
+}
