214f2e207d5fd10e7155d43986fc464f7ca9786a
Aleia DeVore <2496@holbertonschool.com>
2020-09-18 04:27:02 +0000

    Updated variable type

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main  - prints numbers
+ *
+ * Description: prints list of numbers beginning with 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	short a;
+
+	for (a = '0' ; a <= '9' ; a++)
+	{
+		putchar(a);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
