8ad17ad52ebb271c528e403d03f61e320251449e
Aleia DeVore <2496@holbertonschool.com>
2020-09-20 04:07:01 +0000

    Used modulum to print double digits using putchar

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main  - prints numbers
+ *
+ * Description: prints list of numbers with commas
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 99 ; a++)
+	{
+		putchar(a/10 + '0');
+		putchar(a%10 + '0');
+
+		if (a < 99)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
