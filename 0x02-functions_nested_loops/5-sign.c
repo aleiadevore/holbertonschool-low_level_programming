97caef51054ab1cee49a2f7b9d6562a8d33584f6
Aleia DeVore <2496@holbertonschool.com>
2020-09-22 04:03:16 +0000

    Created program to determine if number is positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+int _putchar(char c);
+/**
+ * print_sign - Prints sign of number
+ * @n: Number
+ * Description: Checks 'n' and prints the sign
+ *
+ * Return: n
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return ('-');
+	}
+	return(0);
+}
