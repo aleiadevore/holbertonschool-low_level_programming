99f5304cbead76b86bf0dc7d558820ee6a79bc52
Aleia DeVore <2496@holbertonschool.com>
2020-09-22 01:56:39 +0000

    Created program to print 'holberton' from local header file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+int _putchar(char c);
+#include <stdio.h>
+/**
+ * main  - prints alphabet
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
