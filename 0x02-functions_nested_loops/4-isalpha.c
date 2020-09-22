359dbfd2e481ada5ed990869674184710bac45f8
Aleia DeVore <2496@holbertonschool.com>
2020-09-22 03:33:29 +0000

    Created program to say if 'c' is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+int _putchar(char c);
+/**
+ * _isalpha - Checks value of c
+ * @c: Character that may be an alpha
+ * Description: Outputs 1 if c is alpha and 1 if not
+ *
+ * Return: c
+ */
+
+
+int _isalpha(int c)
+{
+	if(c > 65 || c < 90)
+	{
+		return (1);
+	}
+	else if(c > 97 || c < 122)
+		{
+			return (1);
+		}
+	else
+	{
+		return (0);
+	}
+	return (c);
+}
