e7c2093ba717cdd2b508c5f573126bbeb6ce17b9
Aleia DeVore <2496@holbertonschool.com>
2020-09-22 03:16:10 +0000

    Updated return value

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -17,15 +17,11 @@ int _islower(int c)
 /* Use if statement to say if it is greater or less than 90 */
 	if (c > 90)
 	{
-		_putchar(1);
+		return 1;
 	}
 	if (c <= 90)
 	{
-		_putchar(0);
+		return 0;
 	}
-/*
- * If returning 'c', will return the value of c. Need to specify that it's
- * returning the output.
-*/
 	return (c);
 }
