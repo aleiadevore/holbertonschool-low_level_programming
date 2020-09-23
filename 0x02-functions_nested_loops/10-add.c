225e37fbc2fb08eb2b3e07caa5fffeda3e6ee4ac
Aleia DeVore <2496@holbertonschool.com>
2020-09-23 01:27:50 +0000

    Created program to add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ * @int: integar to add
+ * Description: Adds two integers and returns the result
+ *
+ * Return: Value of integers added together
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = (a + b);
+
+	return (c);
+}
