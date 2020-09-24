b753579d4f95fa709edd463b45924a2917479d7f
Aleia DeVore <2496@holbertonschool.com>
2020-09-24 18:52:00 +0000

    Created program to print ten rows of numbers to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints numbers 0-9 ten times
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int row, col, b, c;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 14; col++)
+		{
+			b = col / 10;
+			c = col % 10;
+
+			if (col <= 9)
+			{
+				_putchar('0' + col);
+			}
+			if (col > 9)
+			{
+				_putchar('0' + b);
+				_putchar('0' + c);
+			}
+		}
+		_putchar('\n');
+	}
+}
