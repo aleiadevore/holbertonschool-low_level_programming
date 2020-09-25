1afebcf348fa6abcab923fdbf74f51229f1fff1e
Aleia DeVore <2496@holbertonschool.com>
2020-09-25 02:41:02 +0000

    Removed extra space in front

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,7 +16,7 @@ void print_triangle(int size)
 	{
 		for (row = 0; row < size; row++)
 		{
-			for (spc = 0; spc <= columns; spc++)
+			for (spc = 0; spc < columns; spc++)
 			{
 				_putchar(' ');
 			}
