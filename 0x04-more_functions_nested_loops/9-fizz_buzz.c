a845e70bf2ce90d143e84558cf279dc1133584ec
Aleia DeVore <2496@holbertonschool.com>
2020-09-25 01:18:02 +0000

    Added space after FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,7 @@ int main(void)
 		else if (i % 5 == 0 && i % 3 != 0)
 			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else if (i == 100)
 			printf("%d", i);
 		else
