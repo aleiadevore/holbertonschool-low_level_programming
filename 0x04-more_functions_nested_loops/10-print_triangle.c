90f2ea29fbf1505a75f893660cf2524264604d89
Aleia DeVore <2496@holbertonschool.com>
2020-09-25 02:39:23 +0000

    Created program to make right-alligned triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of triangle
+ * return: void
+ */
+
+void print_triangle(int size)
+{
+	int row, spc, hash, columns;
+
+	columns = size - 1;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (spc = 0; spc <= columns; spc++)
+			{
+				_putchar(' ');
+			}
+			columns--;
+
+			for (hash = 0; hash <= row; hash++)
+			{
+			_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
