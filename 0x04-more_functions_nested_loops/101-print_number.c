70111e230b56e8695baa64f970233e10ac1d4620
Aleia DeVore <2496@holbertonschool.com>
2020-09-25 03:32:04 +0000

    Updated to include negative numbers to ten

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,11 +8,15 @@
 
 void print_number(int n)
 {
-	int a = (n * -1), d = (n / 1000), e = ((n - (d * 1000)) % 100),
-	b = (n / 100), c = (n % 100);
+	int a = (n * -1), b = (n / 100), c = (n % 100);
 
-	if (n < 10 && n > -10)
+	if (n < 10 && n > 0)
 		_putchar('0' + n);
+	if (n < 0 && n > -10)
+	{
+		_putchar('-');
+		_putchar('0' + -n);
+	}
 	if (n >= 10 && n < 100)
 	{
 		_putchar('0' + n / 10);
@@ -36,12 +40,5 @@ void print_number(int n)
 		_putchar('0' + -b);
 		_putchar('0' + (-c) / 10);
 		_putchar('0' + (-c) % 10);
-	}
-	if (n >= 1000 && n < 10000)
-	{
-		_putchar('0' + d);
-		_putchar('0' + e);
-		_putchar('0' + c / 10);
-		_putchar('0' + c % 10);
 	}
 }
