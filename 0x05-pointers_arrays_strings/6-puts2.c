c88bbdc16cbe55b2056aee9b9892620cf7aefdcd
Aleia DeVore <2496@holbertonschool.com>
2020-09-29 22:04:06 +0000

    Created function to print every other line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other line in a string
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str % 2 == 0)
+			_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
