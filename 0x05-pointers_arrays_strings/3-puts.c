748dad489f1f1d9061c02f601f9b53107bc9dfeb
Aleia DeVore <2496@holbertonschool.com>
2020-09-29 17:51:30 +0000

    Created function to print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints string
+ * @str: string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
