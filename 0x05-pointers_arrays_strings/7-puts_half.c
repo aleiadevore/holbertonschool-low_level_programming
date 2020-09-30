050d2a0d9570143c4ed6eafe176e41f9e4c43530
Aleia DeVore <2496@holbertonschool.com>
2020-09-30 16:14:11 +0000

    Creates program to print second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void puts_half(char *str)
+{
+	int length = 0, half;
+
+        while (str[length] != '\0')
+        {
+                length++;
+        }
+	if (length % 2 == 0)
+	{
+		half = length / 2;
+		while(half <= length)
+		{
+			_putchar(str[half]);
+			half++;
+		}
+	}
+	else
+	{
+		half = (length - 1) / 2;
+		while(half <= length)
+		{
+			_putchar(str[half]);
+			half++;
+		}
+	}
+	_putchar('\n');
+}
