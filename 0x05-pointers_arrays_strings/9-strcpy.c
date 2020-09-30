13e0538317f1ad42b1e0430694ca4067dca6a41e
Aleia DeVore <2496@holbertonschool.com>
2020-09-30 03:38:23 +0000

    Created function to copy pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies string to buffer
+ * @dest: destination
+ * @src: source
+ * Return: destination value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
