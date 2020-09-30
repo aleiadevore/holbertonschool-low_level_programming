d0504a01fef290657f24ff914238e81e2f58c915
Aleia DeVore <2496@holbertonschool.com>
2020-09-30 01:58:35 +0000

    Created function to print elements of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_array - prints elements of array to n
+ * @a: array
+ * @n: element
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int e;
+
+	for (e = 0; e <= n; e++)
+	{
+		if (e != n)
+			printf("%d, ", a[e]);
+		else
+			printf("%d", a[e]);
+	}
+	putchar('\n');
+}
