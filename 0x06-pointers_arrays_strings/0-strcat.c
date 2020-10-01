9c68f77d0d292440c73638bedd17eb024328b66d
Aleia DeVore <2496@holbertonschool.com>
2020-10-01 21:03:01 +0000

    Updated array value

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,7 +18,7 @@ char *_strcat(char *dest, char *src)
 	srclength--;
 	for (i = 0 ; i <= srclength && src[i] != '\0' ; i++)
 		dest[srclength + i] = src[i];
-	dest[destlength + i] = '\0';
+	dest[srclength + i] = '\0';
 
 	return (dest);
 }
