9c618efff678e3542dcfa057efac8e62626eb189
Aleia DeVore <2496@holbertonschool.com>
2020-10-02 00:53:24 +0000

    Created function to compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: negative, zero, or positive value if s1 is greater than, equal to,
+ * or less than s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0, b = 0, c;
+
+	while (s1[a] != '\0')
+		a++;
+	while (s2[b] != '\0')
+		b++;
+	if (a > b)
+	{
+		c = 15;
+	}
+	else if (a == b)
+	{
+		c = 0;
+	}
+	else
+	{
+		c = -15;
+	}
+	return (c);
+}
