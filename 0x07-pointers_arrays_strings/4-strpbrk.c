bcb89c944bf43e1d174719d5ea4a5c43f48f7348
Aleia DeVore <2496@holbertonschool.com>
2020-10-05 23:09:12 +0000

    Created function to put pointer at first matching character

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - places pointer at first matching character
+ * @s: string to be evaluated
+ * @accept: string to match
+ * Return: pointer s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n;
+
+	for (i = 0; *s != '\0' && *s != accept[n]; i++)
+	{
+		for (n = 0; s[i] != accept[n] && accept[n] != '\0'; n++)
+			;
+		s++;
+	}
+	return (s);
+}
