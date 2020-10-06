280b55bbebf94d21cf1a4af943eb4ceccf15671c
Aleia DeVore <2496@holbertonschool.com>
2020-10-06 22:37:46 +0000

    Updated for null value

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,18 +11,16 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, n = 0;
 
-	if (*accept != 0 && *s != 0)
+	for (i = 0; *s != '\0'; i++)
 	{
-		for (i = 0; *s != '\0' && *s != accept[n]; i++)
-		{
-			for (n = 0; s[i] != accept[n] && accept[n] != '\0'; n++)
-				;
-			s++;
-		}
-		return (s);
+		for (n = 0; accept[n] != '\0'; n++)
+			if (s[i] == accept[n])
+				break;
+		if (accept[n] != '\0')
+			return (s);
+		else if (accept[n] == '\0' && s[i + 1] == '\0')
+			break;
+		s++;
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
