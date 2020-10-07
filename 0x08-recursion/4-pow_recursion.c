d81a6d077686339a3d475524fb32e58cfe033bf2
Aleia DeVore <2496@holbertonschool.com>
2020-10-07 18:25:41 +0000

    Created program to return exponent

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns x to the power of y
+ * @x: first integer
+ * @y: second integer
+ * Return: x to the power of y or -1 if lower than 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, (y - 1)));
+}
