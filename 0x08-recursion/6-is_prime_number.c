fb6615a69a0c6a1fefdf11e2d7d03a6de7833c11
Aleia DeVore <2496@holbertonschool.com>
2020-10-08 03:30:58 +0000

    Created program to determine if number is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - determines if integer is a prime number
+ * @n: integer
+ * @y: dividend
+ * Return: 1 if prime number, 0 if not
+ */
+
+int prime(int n, int y);
+
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (0);
+		n = prime(n, 2);
+		if (n > 0)
+			return (1);
+		else if (n == 0)
+			return (0);
+		else
+			return (0);
+}
+/**
+ * prime - returns lowest dividend of n
+ * @n: number
+ * @y: dividend
+ * Return: lowest dividend
+ */
+
+int prime(int n, int y)
+{
+	if (y >= n)
+		return (1);
+	else if (n % y == 0)
+		return (n);
+	else if (n % n == 0)
+		return (0);
+	else
+		return (prime(n, y + 1));
+}
