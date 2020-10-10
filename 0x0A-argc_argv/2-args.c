78f57c549b18489f098f28c847c419729411980c
Aleia DeVore <2496@holbertonschool.com>
2020-10-10 21:50:28 +0000

    Created program to print commands on new lines

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints each command on a new line
+ * @argc: number of commands
+ * @argv: array of commands
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[count]);
+	return (0);
+}
