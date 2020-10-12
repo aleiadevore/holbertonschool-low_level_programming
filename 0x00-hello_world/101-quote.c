47a840262d582f9fcfa18451127d7c273f367996
Aleia DeVore <2496@holbertonschool.com>
2020-10-12 20:55:32 +0000

    Created function to print string without putchar or put

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,67 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints string
+ * Return: standard error
+ *
+ */
 
 int main(void)
 {
-	int a = 0;
-
-	putchar(97);
-	putchar(110);
-	putchar(100);
-	putchar(32);
-	putchar(116);
-	putchar(104);
-	putchar(97);
-	putchar(116);
-	putchar(32);
-	putchar(112);
-	putchar(105);
-	putchar(101);
-	putchar(99);
-	putchar(101);
-	putchar(32);
-	putchar(111);
-	putchar(102);
-	putchar(32);
-	putchar(97);
-	putchar(114);
-	putchar(116);
-	putchar(32);
-	putchar(105);
-	putchar(115);
-	putchar(32);
-	putchar(117);
-	putchar(115);
-	putchar(101);
-	putchar(102);
-	putchar(117);
-	putchar(108);
-	putchar(34);
-	putchar(32);
-	putchar(45);
-	putchar(32);
-	putchar(68);
-	putchar(111);
-	putchar(114);
-	putchar(97);
-	putchar(32);
-	putchar(75);
-	putchar(111);
-	putchar(114);
-	putchar(112);
-	putchar(97);
-	putchar(114);
-	putchar(44);
-	putchar(32);
-	putchar(50);
-	putchar('0' + a);
-	putchar(49);
-	putchar(53);
-	putchar(45);
-	putchar(49);
-	putchar(48);
-	putchar(45);
-	putchar(49);
-	putchar(57);
-	putchar('\n');
+	/*find length of array*/
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		/*use write command like they use in putchar file*/
+	write(2, str, sizeof(str) - 1);
 	return (1);
 }
