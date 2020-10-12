45d7ce87f25e381cf745a5e014a5dd905c9d2773
Aleia DeVore <2496@holbertonschool.com>
2020-10-12 21:18:37 +0000

    Created program to print non-repetitive numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints non-repeating numbers
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <=9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			if (x == 8 && y == 9)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
