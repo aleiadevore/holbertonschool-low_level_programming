6aac8eea13bea7ae4b0abd1b8f076d2def8c251a
Aleia DeVore <2496@holbertonschool.com>
2020-10-13 18:36:31 +0000

    Created program to duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a new string
+ * @str: string to duplicate
+ * Return: pointer or null if null
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int x;
+	char  *ptr;
+
+	ptr = malloc(sizeof(str) + 1);
+	for (x = 0; x <= sizeof(str); x++)
+	{
+		ptr[x] = str[x];
+	}
+	return (ptr);
+}
