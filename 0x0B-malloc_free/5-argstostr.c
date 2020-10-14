3eec97dc2e837f67dbec22b3898949dfb75e2dfa
Aleia DeVore <2496@holbertonschool.com>
2020-10-14 20:17:24 +0000

    Created function to concatenate all commands

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -3,14 +3,14 @@
 
 /**
  * argstostr - concatenates all programs in directory
- * @ar:
- * @av:
+ * @ac: number of commands
+ * @av: array of commands
  * Return: pointer or NULL
  */
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, buffer = 0; length = 0;
+	int i, j, buffer = 0, length = 0;
 	char *ptr;
 
 	if (ac == 0)
@@ -19,12 +19,26 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 
 	/*find length of every command*/
+	for (i = 0; av[i] != '\0'; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+	}
 	/*allocate size of char times length plus ac(for new lines) plus 1*/
+	ptr = malloc(sizeof(char) * length + ac + 1);
 	/*return null if no space*/
+	if (!ptr)
+		return (NULL);
 	/*nested for loop to create array*/
-	/*outer loop works with av[i]*/
-	/*inner loop works with buffer*/
-	/*each new command(buffer) means a new line*/
+	for (i = 0; av[i] != '\0'; i++, buffer++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, buffer++)
+			ptr[buffer] = av[i][j];
+/*each new command(buffer) means a new line*/
+		ptr[buffer] = '\n';
+}
 	/*final pointer at buffer is a null byte*/
+	ptr[buffer] = '\0';
 	/*return pointer*/
+	return (ptr);
 }
