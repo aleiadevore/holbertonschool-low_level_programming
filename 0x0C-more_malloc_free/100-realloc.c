0d39cdc6760cc9b163c2a6531b8329172ac127aa
Aleia DeVore <2496@holbertonschool.com>
2020-10-16 02:42:17 +0000

    Coded out fringe options for realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates memory for string
+ * @old_size: old memory size
+ * @new_size: new memory size
+ * Return: pointer to string or NULL on failure
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int *new;
+
+	if (old_size >= new_size)
+		return(ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		exit(98);
+	}
+	if (ptr == NULL)
+	{
+		new = malloc(new_size);
+		return(new);
+	}
+	return (ptr);
+}
