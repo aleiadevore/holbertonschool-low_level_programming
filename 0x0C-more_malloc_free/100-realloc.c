9a6004497b21384c8b2a4aee5b1713236ee399c8
Aleia DeVore <2496@holbertonschool.com>
2020-10-16 02:43:32 +0000

    Fixed formatting

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int *new;
 
 	if (old_size >= new_size)
-		return(ptr);
+		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -22,7 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		new = malloc(new_size);
-		return(new);
+		return (new);
 	}
 	return (ptr);
 }
