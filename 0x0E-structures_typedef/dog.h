cfc5664e70b5dda31f56edb7e22497c7f3448b78
Aleia DeVore <2496@holbertonschool.com>
2020-10-19 20:13:35 +0000

    Created structure and defined new type as new name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 /**
- * struct dog - structure for dog info
+* struct dog - structure for dog info
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
@@ -15,8 +10,15 @@ void free_dog(dog_t *d);
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
-}
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
