7d024641019387974ef84f6e665e3f9e94275ad2
Aleia DeVore <2496@holbertonschool.com>
2020-10-19 22:07:27 +0000

    Updated from owner to name

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
-	if (d->owner != NULL)
-		printf("Name: %s\n", d->owner);
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
 	else
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	printf("Age: %f\n", d->age);
 	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
 	else
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 
 }
