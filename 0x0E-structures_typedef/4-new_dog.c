ba871cebe84badb3e4abd91208d44972c2e280be
Aleia DeVore <2496@holbertonschool.com>
2020-10-20 01:10:53 +0000

    Updated memory allocation

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,25 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strcpy - copies string to buffer
+ * @dest: destination
+ * @src: source
+ * Return: destination value
+ */
 
+char *_strcpy(char *dest, char *src)
+{
+	int length = 0, spot;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+	for (spot = 0; spot <= length; spot++)
+		dest[spot] = src[spot];
+	return (dest);
+}
 /**
  * new_dog - creates new dog structure
  * @name: name of dog
@@ -8,16 +27,33 @@
  * @owner: owner of dog
  * Return: pointer to structure or NULL
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int nmlen, owlen;
 	struct dog *puppy;
 
 	puppy = malloc(sizeof(struct dog));
-	if (!puppy)
+	if (puppy == NULL)
 		return (NULL);
-	puppy->name = name;
+	for (nmlen = 0; name[nmlen] != '\0'; nmlen++)
+		;
+	puppy->name = malloc(sizeof(char) * nmlen + 1);
+	if (puppy->name == NULL)
+	{
+		free(puppy);
+		return (NULL);
+	}
+	_strcpy(puppy->name, name);
 	puppy->age = age;
-	puppy->owner = owner;
+	for (owlen = 0; owner[owlen] != '\0'; owlen++)
+		;
+	puppy->owner = malloc(sizeof(char) * owlen + 1);
+	if (puppy->owner == NULL)
+	{
+		free(puppy->name);
+		free(puppy);
+		return (NULL);
+	}
+	_strcpy(puppy->owner, owner);
 	return (puppy);
 }
