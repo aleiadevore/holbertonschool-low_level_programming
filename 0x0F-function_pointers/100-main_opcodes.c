ab783618c6117f9dc1edcb625c37e124a2b87a14
Aleia DeVore <2496@holbertonschool.com>
2020-10-22 03:57:09 +0000

    Coded outliers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 1 or 2
+ */
+
+int main(int argc, char **argv)
+{
+	if (argv < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
