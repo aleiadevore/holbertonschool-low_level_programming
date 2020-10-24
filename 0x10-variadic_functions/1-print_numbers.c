1116f6620b89053d8d5017a950f476be85797dc2
Aleia DeVore <2496@holbertonschool.com>
2020-10-24 23:17:16 +0000

    Fixed separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,19 +14,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	int seplen, j;
 
-	for (seplen = 0; separator[seplen] != '\0'; seplen++)
-		;
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
 		if (i < (n - 1) && separator != NULL)
 		{
-			for (j = 0; j < seplen; j++)
-				printf("%c", separator[j]);
+			printf("%s", separator);
 		}
 	}
+	va_end(args);
 	printf("\n");
 }
