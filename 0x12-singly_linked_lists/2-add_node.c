c43c39f16485706c5f56d28551092a9fa45e7440
Aleia DeVore <2496@holbertonschool.com>
2020-10-30 20:25:40 +0000

    Updated to free node on malloc failure

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,9 +15,11 @@ list_t *add_node(list_t **head, const char *str)
 	newnode = malloc(sizeof(list_t));
 	if (newnode == NULL)
 	{
+		free(newnode);
 		return (NULL);
 	}
 	newnode->str = strdup(str);
+	free(str);
 	for (count = 0; str[count] != '\0'; count++)
 		;
 	newnode->len = count;
