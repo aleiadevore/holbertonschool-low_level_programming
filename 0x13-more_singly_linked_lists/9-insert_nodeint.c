9b810febc24ffaa505f02e2c14722bba6bfc11ab
Aleia DeVore <2496@holbertonschool.com>
2020-11-05 04:34:43 +0000

    Accounted for null head

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node, *tmp;
 	unsigned int i = 0;
 
+	if (*head == NULL & idx != 0)
+		return (NULL);
 	node = malloc(sizeof(listint_t));
 	tmp = *head;
 	if (!node)
