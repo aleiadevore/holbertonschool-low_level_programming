8964eb0d7b0fea1277a9bbd7c40b13120984027a
Aleia DeVore <2496@holbertonschool.com>
2020-11-05 01:13:00 +0000

    Created program to insert node at index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts node at index
+ * @head: head of list
+ * @idx: place to add node
+ * @n: int for node
+ * Return: pointer to new node or NULL
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *tmp;
+	unsigned int i = 0;
+
+	node = malloc(sizeof(listint_t));
+	tmp = *head;
+	if (!node)
+		return (NULL);
+	for (i = 0; tmp != NULL && i < (idx - 1); i++)
+	{
+		tmp = tmp->next;
+	}
+	if (idx > (i + 1))
+		return (NULL);
+	node->next = tmp->next;
+	tmp->next = node;
+	node->n = n;
+	return (node);
+}
