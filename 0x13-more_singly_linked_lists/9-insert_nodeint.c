0f17d899c4a3ffc5ee95ddd79b9676f6f5099e7b
Aleia DeVore <2496@holbertonschool.com>
2020-11-05 04:48:47 +0000

    Updated i value

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	for (; tmp != NULL; len++)
 		tmp = tmp->next;
-	if (idx > (len + 1))
+	if (idx > len)
 	{
 		free(node);
 		return (NULL);
