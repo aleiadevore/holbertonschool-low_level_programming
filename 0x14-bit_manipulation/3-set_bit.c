1cf320f2f0d271755fd43d5567dd80d1f7d6e0a2
Aleia DeVore <2496@holbertonschool.com>
2020-11-06 17:13:40 +0000

    Switched to bitwise operators

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,34 +9,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i, j, l, count = 0, val;
-	char c, bit[32];
-
-	if (index > 32)
+	if (index > 64)
 		return (-1);
-	l = *n;
-	for (i = 0; i < 32; i++)
-		bit[i] = 0;
-	for (i = 31; l > 0; i--)
-	{
-		c = (l % 2) + '0';
-		bit[i] = c;
-		l /= 2;
-	}
-	j = 31 - index;
-	bit[j] = '1';
-	for (i = 0; bit[i] != '1'; i++)
-		;
-	for (; i < 32; i++)
-	{
-		if (bit[i] == 48)
-			val = 0;
-		else if (bit[i] == 49)
-			val = 1;
-		else
-			return (-1);
-		count = (count * 2) + val;
-	}
-	*n = count;
+	if (n == NULL)
+		return (-1);
+	*n = *n | 1 << index;
 	return (1);
 }
