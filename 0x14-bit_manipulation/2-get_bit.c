de024df049506221f744be73c9b0754a331c07f2
Aleia DeVore <2496@holbertonschool.com>
2020-11-06 16:30:18 +0000

    Corrected size of unsigned long int

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,19 +10,21 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int i, j;
-	char k, c, bit[32];
+	char k, c, bit[64];
 
+	if (index > 64)
+		return (-1);
 	if (n == 0 && index > 0)
 		return (-1);
-	for (i = 0; i < 32; i++)
+	for (i = 0; i < 64; i++)
 		bit[i] = 0;
-	for (i = 31; n > 0; i--)
+	for (i = 63; n > 0; i--)
 	{
 		c = (n % 2) + '0';
 		bit[i] = c;
 		n /= 2;
 	}
-	for (j = 0, i = 31; j <= index; j++, i--)
+	for (j = 0, i = 63; j <= index; j++, i--)
 		k = bit[i];
 	if (k == 48)
 		j = 0;
