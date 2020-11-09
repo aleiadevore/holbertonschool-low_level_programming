07686edfc15975c18070e40d060337d54e105285
Aleia DeVore <2496@holbertonschool.com>
2020-11-09 23:38:07 +0000

    Created function to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,14 +9,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, i;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, sizeof(text_content));
+	for (i = 0; text_content[i] != '\0'; i++)
+		write(fd, text_content, 1);
 	close(fd);
 	return (1);
 }
