bb6040cf4e8f54fdc90abc4c54df676f298a99ac
Aleia DeVore <2496@holbertonschool.com>
2020-11-09 23:52:56 +0000

    Closed file on fail

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 	while (*text_content != '\0')
 	{
 		write(fd, text_content, 1);
