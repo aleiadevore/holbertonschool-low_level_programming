918e954e03f3f722e0ad9e2283cff909c99a3e39
Aleia DeVore <2496@holbertonschool.com>
2020-11-09 23:44:56 +0000

    Fixed write errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,15 +9,18 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
 		return (-1);
-	for (i = 0; text_content[i] != '\0'; i++)
+	while (*text_content != '\0')
+	{
 		write(fd, text_content, 1);
+		text_content++;
+	}
 	close(fd);
 	return (1);
 }
