d37b130ff82b4695a1c9df8d1504960f47fab65b
Aleia DeVore <2496@holbertonschool.com>
2020-11-09 23:34:24 +0000

    Created function to create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates file
+ * @filename: name of file
+ * @text_content: NULL terminating string to write to file
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, sizeof(text_content));
+	close(fd);
+	return (1);
+}
