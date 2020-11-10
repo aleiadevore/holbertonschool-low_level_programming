8319fb16c574efa963358fa8a3746f5fa3a6f8b4
Aleia DeVore <2496@holbertonschool.com>
2020-11-10 02:15:47 +0000

    Updated permissions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,19 +12,21 @@ int main(int ac, char **av)
 	int fd1, fd2, f, check;
 	char buff[1024];
 
+	for (f = 0; f <= 1024; f++)
+		buff[f] = '\0';
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd1 = open(av[1], 010);
+	fd1 = open(av[1], O_RDWR, 0664);
 	check = read(fd1, buff, 1024);
 	if (fd1 == -1 || check == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd2 = open(av[1], O_CREAT | O_WRONLY | O_TRUNC, 664);
+	fd2 = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	check = write(fd2, buff, 1024);
 	if (fd2 == -1 || check == -1)
 	{
