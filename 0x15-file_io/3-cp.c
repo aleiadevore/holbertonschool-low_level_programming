c80327530f54d2b3737c8a64951f69fe2c939554
Aleia DeVore <2496@holbertonschool.com>
2020-11-10 03:16:32 +0000

    Updated fail on closing

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,13 +36,13 @@ int main(int ac, char **av)
 	f = close(fd1);
 	if (f == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd1);
+		dprintf(2, "Error: Can't close fd %s\n", av[1]);
 		exit(100);
 	}
 	f = close(fd2);
 	if (f == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd2);
+		dprintf(2, "Error: Can't close fd %s\n", av[2]);
 		exit(100);
 	}
 	return (1);
