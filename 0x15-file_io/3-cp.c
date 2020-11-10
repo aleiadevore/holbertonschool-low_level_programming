5b0748266f3bc80ee9dd6d8f431bf2ad68333f3e
Aleia DeVore <2496@holbertonschool.com>
2020-11-10 03:34:09 +0000

    Fixed print value for failed closure

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,13 +36,13 @@ int main(int ac, char **av)
 	f = close(fd1);
 	if (f == -1)
 	{
-		dprintf(2, "Error: Can't close fd %dn", fd1);
+		dprintf(2, "Error: Can't close fd %dn", f);
 		exit(100);
 	}
 	f = close(fd2);
 	if (f == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd2);
+		dprintf(2, "Error: Can't close fd %d\n", f);
 		exit(100);
 	}
 	return (1);
