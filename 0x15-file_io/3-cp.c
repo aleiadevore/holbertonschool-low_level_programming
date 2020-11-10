819a730732613c9f11f66f9ea08ba27cf345bd9b
Aleia DeVore <2496@holbertonschool.com>
2020-11-10 03:54:43 +0000

    Updated return value

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,27 @@
 #include "holberton.h"
 
 /**
+ * closefd - closes files
+ * @fd: file descriptor
+ * buff: buffer
+ * Return: value of file descriptor
+ */
+
+int closefd(int fd, char *buff)
+{
+	int f;
+
+	f = close(fd);
+	if (f == -1)
+	{
+		free(buff);
+		dprintf(2, "Error: Can't close fd %dn", f);
+		exit(100);
+	}
+	return (f);
+}
+
+/**
  * main - copies contents of one file to another
  * @ac: number of arguments
  * @av: array of arguments
@@ -10,19 +31,21 @@
 int main(int ac, char **av)
 {
 	int fd1, fd2, f, check;
-	char buff[1024];
+	char *buff;
 
-	for (f = 0; f <= 1024; f++)
-		buff[f] = '\0';
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	buff = malloc(1024 * sizeof(char));
+	for (f = 0; f <= 1024; f++)
+		buff[f] = '\0';
 	fd1 = open(av[1], O_RDWR, 0664);
 	check = read(fd1, buff, 1024);
 	if (fd1 == -1 || check == -1)
 	{
+		free(buff);
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
@@ -30,20 +53,12 @@ int main(int ac, char **av)
 	check = write(fd2, buff, 1024);
 	if (fd2 == -1 || check == -1)
 	{
+		free(buff);
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	f = close(fd1);
-	if (f == -1)
-	{
-		dprintf(2, "Error: Can't close fd %dn", f);
-		exit(100);
-	}
-	f = close(fd2);
-	if (f == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", f);
-		exit(100);
-	}
-	return (1);
+	closefd(fd1, buff);
+	closefd(fd2, buff);
+	free(buff);
+	return (0);
 }
