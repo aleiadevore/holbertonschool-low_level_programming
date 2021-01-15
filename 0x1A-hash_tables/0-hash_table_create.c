0f3da6072b2771ed1cddfdc5dd145955ee99ac02
Aleia DeVore <2496@holbertonschool.com>
2021-01-15 19:48:22 +0000

    Malloc'd for array and fixed Betty error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,19 +1,24 @@
 #include "hash_tables.h"
 
-/*
+/**
  * hash_table_create - creates a hash table
  * @size: size of the array
  * Return: pointer to hash table or NULL
- *
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	hash_node_t **newarray;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 	table->size = size;
+	newarray = malloc(sizeof(hash_node_t) * size);
+	for (i = 0; i < size; i++)
+		newarray[i] = NULL;
+	table->array = newarray;
 	return (table);
 }
