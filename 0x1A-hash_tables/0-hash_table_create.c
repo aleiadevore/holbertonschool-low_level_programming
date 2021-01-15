1bf1babf2eee0c7d6ff6640083140ea41d4b1504
Aleia DeVore <2496@holbertonschool.com>
2021-01-15 19:18:35 +0000

    Updated size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
-	table = malloc(sizeof(hash_table_t) * size);
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
+	table->size = size;
 	return (table);
 }
