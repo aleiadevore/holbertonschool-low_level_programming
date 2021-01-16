670f98c4c8341750ebc183161a0a40c3a815c469
Aleia DeVore <2496@holbertonschool.com>
2021-01-16 02:17:53 +0000

    Created function to print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned int i;
+	int b = 0;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			for (node = ht->array[i]; node != NULL; node = node->next)
+			{
+				if (b == 0)
+					printf("'%s': '%s'", node->key, node->value);
+				else
+					printf(", '%s': '%s'", node->key, node->value);
+				b++;
+			}
+		}
+	}
+	printf("}\n");
+}
