6a7a7bfac50e38b24ba5a060a07dd812d351e551
Aleia DeVore <2496@holbertonschool.com>
2021-01-16 02:19:15 +0000

    Accounted for NULL value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,9 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node = NULL, *oldhead;
 	unsigned long int hash_index;
-	char *vcpy = NULL;
 
-	if (key == NULL)
+	if (key == NULL || value == NULL)
 		return (0);
 	if (strlen(key) < 1)
 		return (0);
@@ -22,8 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!node)
 		return (0);
 	node->key = (char *)key;
-	vcpy = strdup((char *)value);
-	node->value = vcpy;
+	node->value = strdup((char *)value);
 	node->next = NULL;
 
 	hash_index = key_index((const unsigned char *)key, ht->size);
