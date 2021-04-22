f8636886b87b73e5bfdd0644980ea0c2476dfa00
Aleia DeVore <2496@holbertonschool.com>
2021-04-22 01:41:25 +0000

    Return correct but prints need fixing

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "search_algos.h"
+
+/**
+ * jump_search - searches sorted array by 'jumping'
+ * @array: array to search
+ * @size: size of array
+ * @value: value to find
+ * Return: pointer to value or -1
+*/
+
+int jump_search(int *array, size_t size, int value)
+{
+	int step = sqrt(size), p = 0, size_i = size, spot = size;
+
+	while (array[spot - 1] < value)
+	{
+		p = step;
+		step += sqrt(size_i);
+		if (p >= size_i)
+			return (-1);
+		if (step < size_i)
+			spot = step;
+		else
+			spot = size_i;
+	}
+	printf("Value found between indexes [%d] and [%d]\n", p, p + step);
+	while (array[p] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", p, array[p]);
+		p++;
+		if (p == spot)
+			return (-1);
+	}
+	if (array[p] == value)
+	{
+		printf("Value checked array[%d] = [%d]\n", p, array[p]);
+		return (p);
+	}
+	return (-1);
+
+}
