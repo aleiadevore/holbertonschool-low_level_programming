e1977f613939954ba7b101f3eaa066bdc75cac1d
aleiadevore <2496@holbertonschool.com>
2021-11-30 16:10:25 -0600

    Fixed redundant code

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -68,33 +68,15 @@ int real_binary(int *arr, int l, int r, int v)
 
 		if (arr[mid] > v)
 		{
-			printf("Going left\n");
+			/*printf("Going left\n");*/
 			return (real_binary(arr, l, mid, v));
 		}
-		printf("Going right\n");
+		/*printf("Going right\n");*/
 		return (real_binary(arr, mid + 1, r, v));
 	}
 
 	return (-1);
 }
-
-/**
- * advanced_binary - searches for int in array by dividing in half
- * @array: array to search
- * @size: size of array
- * @value: value to find
- * Return: index of value or -1
- */
-
-int advanced_binary(int *array, size_t size, int value)
-{
-	int right = size - 1;
-
-	if (array == NULL)
-		return (-1);
-	return (real_binary(array, 0, right, value));
-}
-
 
 /**
  * advanced_binary - searches for int in array by dividing in half
